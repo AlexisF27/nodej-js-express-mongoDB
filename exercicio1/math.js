4212a170762e1388f6562021671ca29074d93869
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-01-14 17:55:22 +0000

    basics|impo directories|exporting custom modules

---
+++ exercicio1/math.js
@@ -0,0 +1,17 @@
+//esta es una de las formas que se puede exportar las funcionalidades
+
+// const add =(a,b) => a+b;
+// const substract =(a,b) => a-b;
+// const multiply =(a,b) => a*b;
+// const divide =(a,b) => a/b;
+
+// module.exports = {add, substract, multiply, divide} 
+
+//-------------------------------------------------------------------------------------
+
+//se puede hacer de forma general añadiendo la palabra reservada exports antes de la const
+
+exports.add = (a, b) => a + b;
+exports.substract = (a, b) => a - b;
+exports.multiply = (a, b) => a * b;
+exports.divide = (a, b) => a / b;
---
+++ exercicio1/server.js
@@ -0,0 +1,38 @@
+// How NodeJs differs from Vanilla JSON 
+//  - Node runs on a server - not in a brownser (backend not fronted)
+//  - The console is the terminal window
+console.log('Hello World')
+// en la consola executar node exercicio1/server (no es necesario la terminacion js)
+
+//  - global objects istead of window object
+console.log(global)
+
+// Has common core modules that we will explore 
+// Common JS modules instead of ES6 modules 
+// its needed the require syntax in order to import modules
+
+const os = require('os');
+const path = require('path');
+
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
+
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+// console.log(path.parse(__filename))
+
+//se le puede hacer destrucutrar el contenido del archivo al cual estoy accediendo y solamente traer sus funciones 
+
+const {add, substract, multiply, divide} = require('./math');
+
+console.log(add(2,3));
+console.log(substract(2,3));
+console.log(multiply(2,3));
+console.log(divide(2,3));
+
+//node js no  tiene algunos JS APIs like fetch
