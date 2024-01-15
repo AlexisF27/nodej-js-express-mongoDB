e72eb175460a83054c7e7db61e67db768e02f1c2
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-01-15 16:09:56 +0000

    dependencies dev and prod- install/unistall

--- exercicio3/index.js
+++ exercicio3/index.js
@@ -1,4 +1,14 @@
-console.log("!")
+//console.log("!")
 //NOTE: npm install nodemon -g  te permite correro los ficheros sin necesidad de cargarlos node exercicio3/index
 //  aunque si lo hago desde la carpeta padre, aun tengo que especificar la direccion del fichero pero sin el fichero
 //  nodemon exercicio3
+
+const {format} = require('date-fns');
+const { v4: uuid} = require('uuid');
+
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
+
+console.log(uuid());
+console.log("HEllo");
+//NOTE:
+//  npm i nodemon --save-dev that is how we install devdependencies 
