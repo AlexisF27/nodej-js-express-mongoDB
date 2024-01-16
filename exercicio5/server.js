0cf80fcebebd31d01b454a4a6b466c80c73b93d7
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-01-16 13:54:45 +0000

    Server starting on port 3500

--- exercicio5/server.js
+++ exercicio5/server.js
@@ -1,18 +1,31 @@
 const logEvents = require('./logEvents');
-
+const http = require('http');
+const path = require('path');
+const fs = require ('fs');
+const fsPromises = require ('fs').promises;
 const EventEmmiter = require('events');
 
-class MyEmmiter extends EventEmmiter {};
+class Emmiter extends EventEmmiter {};
 
 
 //initialize object
-const myEmmiter = new MyEmmiter();
+const myEmmiter = new Emmiter();
+
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((request,response) => {
+  console.log(request.url, request.method);
+})
+
+server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`)); 
+
 
 // add listener for the log event
 
-myEmmiter.on('log', (msg) => logEvents(msg));
+// myEmmiter.on('log', (msg) => logEvents(msg));
 
-setTimeout(() => {
-  //Emit event
-  myEmmiter.emit('log', 'Log event emmiter');
-},2000)
+// setTimeout(() => {
+//   //Emit event
+//   myEmmiter.emit('log', 'Log event emmiter');
+// },2000)
