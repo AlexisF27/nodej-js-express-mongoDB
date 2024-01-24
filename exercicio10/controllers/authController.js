d15138e67b93b94ee9ba0c1759354aaaf3f89741
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-01-24 20:10:02 +0000

    Auth password done

--- exercicio10/controllers/authController.js
+++ exercicio10/controllers/authController.js
@@ -3,12 +3,17 @@ const userDB = {
   setUsers: function (data) { this.users = data }
 }
 
-const fsPromises = require('fs').promises;
-const path = require('path');
 const bcrypt = require('bcrypt');
 
-const handleLogin = async (request,response) => {
+const handleLogin = async (request, response) => {
+  const { user, pwd } = request.body;
+  if (!user || !pwd) return response.status(400).json({ 'message': 'Username and password are required' });
+  const foundUser = userDB.users.find(person => person.username === user);
+  if (!foundUser) return response.sendStatus(401); //Unauthorized
+  //NOTE: evaluate password
 
+  const match = await bcrypt.compare(pwd, foundUser.password);
+  if (match ? response.json({ 'success': `User ${user} is looger in` }) : response.sendStatus(401));
 }
 
-module.exports = handleLogin;
+module.exports = {handleLogin};
--- exercicio10/server.js
+++ exercicio10/server.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
 app.use('/employees', require('./routes/api/employees'));
 
 
